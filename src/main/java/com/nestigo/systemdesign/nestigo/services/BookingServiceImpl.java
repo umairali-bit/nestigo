34e05f5adf1668588458a5cb0526372bebff70f0
Umair Ali <umairmamoor@gmail.com>
2026-01-20 02:14:47 -0500

    initiatePayment()

--- src/main/java/com/nestigo/systemdesign/nestigo/services/BookingServiceImpl.java
+++ src/main/java/com/nestigo/systemdesign/nestigo/services/BookingServiceImpl.java
@@ -3,25 +3,20 @@ package com.nestigo.systemdesign.nestigo.services;
 import com.nestigo.systemdesign.nestigo.dtos.BookingDTO;
 import com.nestigo.systemdesign.nestigo.dtos.BookingRequestDTO;
 import com.nestigo.systemdesign.nestigo.dtos.GuestDTO;
-import com.nestigo.systemdesign.nestigo.dtos.HotelDTO;
 import com.nestigo.systemdesign.nestigo.entities.*;
 import com.nestigo.systemdesign.nestigo.entities.enums.BookingStatus;
 import com.nestigo.systemdesign.nestigo.exceptions.ResourceNotFoundException;
 import com.nestigo.systemdesign.nestigo.exceptions.UnauthorizedException;
 import com.nestigo.systemdesign.nestigo.repositories.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import lombok.*;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.catalina.User;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
@@ -39,6 +34,10 @@ public class BookingServiceImpl implements BookingService {
     private final UserRepository userRepository;
     private final GuestRepository guestRepository;
     private final ModelMapper modelMapper;
+    private final CheckoutService checkoutService;
+
+    @Value("${frontend.url}")
+    private String frontEndUrl;
 
     @Override
     @Transactional
@@ -48,32 +47,31 @@ public class BookingServiceImpl implements BookingService {
                 bookingRequestDTO.getRoomId(), bookingRequestDTO.getCheckInDate(), bookingRequestDTO.getCheckOutDate());
 
         HotelEntity hotel = hotelRepository.findById(bookingRequestDTO.getHotelId()).orElseThrow(() ->
-                new ResourceNotFoundException("Hotel NOT found with id:"+ bookingRequestDTO.getHotelId()));
+                new ResourceNotFoundException("Hotel NOT found with id:" + bookingRequestDTO.getHotelId()));
 
         RoomEntity room = roomRepository.findById(bookingRequestDTO.getRoomId()).orElseThrow(() ->
-                new ResourceNotFoundException("Room NOT found with id:"+ bookingRequestDTO.getRoomId()));
+                new ResourceNotFoundException("Room NOT found with id:" + bookingRequestDTO.getRoomId()));
 
         List<InventoryEntity> inventoryList = inventoryRepository.findAndLockInventory(room.getId(),
-                bookingRequestDTO.getCheckInDate(), bookingRequestDTO.getCheckOutDate(),bookingRequestDTO.getRoomsCount());
+                bookingRequestDTO.getCheckInDate(), bookingRequestDTO.getCheckOutDate(), bookingRequestDTO.getRoomsCount());
 
-        long daysCount = ChronoUnit.DAYS.between(bookingRequestDTO.getCheckInDate(), bookingRequestDTO.getCheckOutDate())+1;
+        long daysCount = ChronoUnit.DAYS.between(bookingRequestDTO.getCheckInDate(), bookingRequestDTO.getCheckOutDate()) + 1;
         if (inventoryList.size() != daysCount) {
             throw new IllegalStateException("Room is not available");
         }
 
 //      reserving the room
-         for(InventoryEntity inventory : inventoryList) {
-             inventory.setReservedCount(inventory.getReservedCount()+ bookingRequestDTO.getRoomsCount());
-         }
+        for (InventoryEntity inventory : inventoryList) {
+            inventory.setReservedCount(inventory.getReservedCount() + bookingRequestDTO.getRoomsCount());
+        }
 
-         inventoryRepository.saveAll(inventoryList);
-
+        inventoryRepository.saveAll(inventoryList);
 
 
         //TODO: calculate the dynamic price
 
-         //create a booking
-        BookingEntity booking =BookingEntity.builder()
+        //create a booking
+        BookingEntity booking = BookingEntity.builder()
                 .bookingStatus(BookingStatus.RESERVED)
                 .hotel(hotel)
                 .room(room)
@@ -96,23 +94,22 @@ public class BookingServiceImpl implements BookingService {
         log.info("Adding guests for booking with id: {}", bookingId);
 
         BookingEntity booking = bookingRepository.findById(bookingId).orElseThrow(() ->
-                new ResourceNotFoundException("Booking NOT found with id:"+ bookingId));
+                new ResourceNotFoundException("Booking NOT found with id:" + bookingId));
 
         UserEntity user = getCurrentUser();
 
-        if(!user.equals(booking.getUser())) {
-            throw new UnauthorizedException("Booking does not belong to this user" +user.getId());
+        if (!user.equals(booking.getUser())) {
+            throw new UnauthorizedException("Booking does not belong to this user" + user.getId());
 
         }
 
 
-
-        if(hasBookingExpired(booking)){
+        if (hasBookingExpired(booking)) {
             throw new IllegalStateException("Booking has expired");
 
         }
 
-        if(booking.getBookingStatus() != BookingStatus.RESERVED){
+        if (booking.getBookingStatus() != BookingStatus.RESERVED) {
             throw new IllegalStateException("Booking is not under reserved state");
         }
 
@@ -133,22 +130,29 @@ public class BookingServiceImpl implements BookingService {
     public String initiatePayments(Long bookingId) {
 //        fetching booking
         BookingEntity booking = bookingRepository.findById(bookingId).orElseThrow(
-                () -> new ResourceNotFoundException("Booking NOT found with id:"+ bookingId));
+                () -> new ResourceNotFoundException("Booking NOT found with id:" + bookingId));
 
 //        fetching the user
         UserEntity user = getCurrentUser();
 
 //          checking if the user is the same
-        if(!user.equals(booking.getUser())) {
-            throw new UnauthorizedException("Booking does not belong to this user" +user.getId());
+        if (!user.equals(booking.getUser())) {
+            throw new UnauthorizedException("Booking does not belong to this user" + user.getId());
         }
 
 //        checking if the booking is expired or not
-        if(hasBookingExpired(booking)){
+        if (hasBookingExpired(booking)) {
             throw new IllegalStateException("Booking has expired");
 
         }
-        return "";
+
+        String sessionUrl = checkoutService.getCheckoutSession(booking,
+                frontEndUrl + "payments/success", frontEndUrl + "payments/failure");
+
+        booking.setBookingStatus(BookingStatus.PAYMENT_PENDING);
+        bookingRepository.save(booking);
+
+        return sessionUrl;
     }
 
     public boolean hasBookingExpired(BookingEntity bookingDTO) {
