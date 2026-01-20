67ba04c2c27b9e3ad632bb763c6c8b71ed28f0df
Umair Ali <umairmamoor@gmail.com>
2026-01-20 01:40:03 -0500

    setting session.getId to PaymentSessionId, saving the booking and returning sessionUrl

--- src/main/java/com/nestigo/systemdesign/nestigo/services/CheckoutServiceImp.java
+++ src/main/java/com/nestigo/systemdesign/nestigo/services/CheckoutServiceImp.java
@@ -2,6 +2,7 @@ package com.nestigo.systemdesign.nestigo.services;
 
 
 import com.nestigo.systemdesign.nestigo.entities.BookingEntity;
+import com.nestigo.systemdesign.nestigo.repositories.BookingRepository;
 import com.nestigo.systemdesign.nestigo.entities.UserEntity;
 import com.stripe.exception.StripeException;
 import com.stripe.model.Customer;
@@ -9,12 +10,16 @@ import com.stripe.model.checkout.Session;
 import com.stripe.param.CustomerCreateParams;
 import com.stripe.param.checkout.SessionCreateParams;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class CheckoutServiceImp implements CheckoutService {
+
+    private final BookingRepository bookingRepository;
 
 
     @Override
@@ -53,11 +58,14 @@ public class CheckoutServiceImp implements CheckoutService {
 
             Session session = Session.create(sessionParams);
 
+            booking.setPaymentSessionId(session.getId());
+            bookingRepository.save(booking);
+
+            return session.getUrl();
+
         } catch (StripeException e) {
             throw new RuntimeException(e);
         }
 
-
-        return "";
     }
 }
