95513c97a8e31ce4304c21933c4a18a3ae20e886
Umair Ali <umairmamoor@gmail.com>
2026-01-23 06:32:05 -0500

    confirmed booking query

--- src/main/java/com/nestigo/systemdesign/nestigo/repositories/InventoryRepository.java
+++ src/main/java/com/nestigo/systemdesign/nestigo/repositories/InventoryRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -56,6 +57,23 @@ public interface InventoryRepository extends JpaRepository<InventoryEntity, Long
             @Param("endDate") LocalDate endDate,
             @Param("roomsCount") Integer roomsCount
     );
+
+    @Modifying
+    @Query("""
+            UPDATE InventoryEntity i 
+            SET i.reservedCount = i.reservedCount - :numberOfRooms,
+                i.bookedCount = i.bookedCount + :numberOfRooms
+            WHERE i.room.id = :roomId
+                AND i.date BETWEEN :startDate AND :endDate
+                AND (i.totalCount - i.bookedCount) >= :numberOfRooms
+                AND i.reservedCount >= :numberOfRooms
+                AND i.closed = false         
+            """)
+    void confirmedBooking(@Param("roomId") Long roomId,
+                          @Param("startDate") LocalDate startDate,
+                          @Param("endDate") LocalDate endDate,
+                          @Param("numberOfRooms") int numberOfRooms
+                          );
 
 
     List<InventoryEntity> findByHotelAndDateBetween(HotelEntity hotelEntity, LocalDate startDate, LocalDate endDate);
