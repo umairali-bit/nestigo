17d67d9be78d720c67ff0ceb6b834dd4982edeaf
Umair Ali <umairmamoor@gmail.com>
2026-01-25 08:32:58 -0500

    returning price of the inventory list

--- src/main/java/com/nestigo/systemdesign/nestigo/strategy/PricingService.java
+++ src/main/java/com/nestigo/systemdesign/nestigo/strategy/PricingService.java
@@ -14,12 +14,21 @@ public class PricingService {
     public BigDecimal calculateDynamicPricing(InventoryEntity inventory) {
         PricingStrategy pricingStrategy = new BasePriceStrategy();
 
-        // apply the additional strategies
+//  apply the additional strategies
         pricingStrategy = new SurgePricingStrategy(pricingStrategy);
         pricingStrategy = new OccupancyPricingStrategy(pricingStrategy);
         pricingStrategy = new UrgencyPricingStrategy(pricingStrategy);
         pricingStrategy = new HolidayPricingStrategy(pricingStrategy);
 
         return pricingStrategy.calculatePrice(inventory);
+    }
+
+//    returning price of the inventory list
+    public BigDecimal calculateDynamicPricing(List<InventoryEntity> inventoryEntityList) {
+        return inventoryEntityList.stream()
+                .map(inventory -> calculateDynamicPricing(inventory))
+                .reduce(BigDecimal.ZERO, (a, b) -> a.add(b));
+
+
     }
 }
