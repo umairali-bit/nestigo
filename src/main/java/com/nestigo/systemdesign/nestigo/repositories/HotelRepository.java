737f5e30eeb3e5966d6e68aeb8282d7047e69545
Umair Ali <umairmamoor@gmail.com>
2026-02-10 03:16:40 -0500

    Admin api - getAllHotels (repository method)

--- src/main/java/com/nestigo/systemdesign/nestigo/repositories/HotelRepository.java
+++ src/main/java/com/nestigo/systemdesign/nestigo/repositories/HotelRepository.java
@@ -1,9 +1,13 @@
 package com.nestigo.systemdesign.nestigo.repositories;
 
 import com.nestigo.systemdesign.nestigo.entities.HotelEntity;
+import com.nestigo.systemdesign.nestigo.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface HotelRepository extends JpaRepository<HotelEntity, Long> {
+    List<HotelEntity> findByOwner(UserEntity user);
 }
