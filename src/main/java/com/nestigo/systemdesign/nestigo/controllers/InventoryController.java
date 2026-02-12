f7bdf0139ac2a68240a18dc04b6ca276ee240b44
Umair Ali <umairmamoor@gmail.com>
2026-02-12 02:28:44 -0500

    inventory of the room - controller

---
+++ src/main/java/com/nestigo/systemdesign/nestigo/controllers/InventoryController.java
@@ -0,0 +1,35 @@
+package com.nestigo.systemdesign.nestigo.controllers;
+
+
+import com.nestigo.systemdesign.nestigo.services.InventoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin/inventory")
+@RequiredArgsConstructor
+public class InventoryController {
+
+    private final InventoryService inventoryService;
+
+    @GetMapping("/rooms/{roomId}")
+    public ResponseEntity<List<InventoryDTO>> getAllInventoryByRoom(@PathVariable Long roomId) {
+        return ResponseEntity.ok(inventoryService.getAllInventoryByRoom(roomId));
+    }
+
+
+
+
+
+
+
+
+
+
+}
