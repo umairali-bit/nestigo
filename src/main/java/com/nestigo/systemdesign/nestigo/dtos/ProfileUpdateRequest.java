26b3632d283802bf3b59dabf947d0199ca45657a
Umair Ali <umairmamoor@gmail.com>
2026-02-13 05:43:33 -0500

    profile - ProfileUpdateRequestDTO

---
+++ src/main/java/com/nestigo/systemdesign/nestigo/dtos/ProfileUpdateRequest.java
@@ -0,0 +1,15 @@
+package com.nestigo.systemdesign.nestigo.dtos;
+
+
+import com.nestigo.systemdesign.nestigo.entities.enums.Gender;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class ProfileUpdateRequest {
+    private String name;
+    private LocalDate dateOfBirth;
+    private Gender gender;
+
+}
