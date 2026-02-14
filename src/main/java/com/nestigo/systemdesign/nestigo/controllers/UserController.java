fe76e87626a9cd041c69d3e8f3f0775ef21cca80
Umair Ali <umairmamoor@gmail.com>
2026-02-14 01:25:18 -0500

    profile - myBookings controller

--- src/main/java/com/nestigo/systemdesign/nestigo/controllers/UserController.java
+++ src/main/java/com/nestigo/systemdesign/nestigo/controllers/UserController.java
@@ -1,14 +1,15 @@
 package com.nestigo.systemdesign.nestigo.controllers;
 
 
+import com.nestigo.systemdesign.nestigo.dtos.BookingDTO;
 import com.nestigo.systemdesign.nestigo.dtos.ProfileUpdateRequestDTO;
+import com.nestigo.systemdesign.nestigo.services.BookingService;
 import com.nestigo.systemdesign.nestigo.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
@@ -16,12 +17,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+    private final BookingService bookingService;
 
     @PutMapping("/profile")
     public ResponseEntity<Void> updateProfile(@RequestBody ProfileUpdateRequestDTO profileUpdateRequestDTO) {
 
         userService.updateProfile(profileUpdateRequestDTO);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/myBookings")
+    public ResponseEntity<List<BookingDTO>> getMyBookings() {
+        return ResponseEntity.ok(bookingService.getMyBookings());
     }
 
 }
