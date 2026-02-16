9cdd6f8641d4523ff2970e94b092f31a296c6136
Umair Ali <umairmamoor@gmail.com>
2026-02-16 07:32:39 -0500

    UserDTO - adding fields

--- src/main/java/com/nestigo/systemdesign/nestigo/dtos/UserDTO.java
+++ src/main/java/com/nestigo/systemdesign/nestigo/dtos/UserDTO.java
@@ -1,9 +1,11 @@
 package com.nestigo.systemdesign.nestigo.dtos;
 
+import com.nestigo.systemdesign.nestigo.entities.enums.Gender;
 import com.nestigo.systemdesign.nestigo.entities.enums.RoleEnum;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.time.LocalDate;
 import java.util.Set;
 
 @Data
@@ -17,6 +19,10 @@ public class UserDTO {
     private String password;//encoded
 
     private String name;
+
+    private Gender gender;
+
+    private LocalDate dateOfBirth;
 
 
     private Set<RoleEnum> roles;
